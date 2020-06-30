9e9600bb80e25a378d6c255dda6623afcd7e107f
Valentin Popov <VOPopov@luxoft.com>
2020-06-30 10:02:08 +0300

    schema.sql, data.sql

---
+++ hw05-library/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into AUTHOR(ID, NAME, SURNAME) values (1, 'Richard', 'Bergman');
+insert into AUTHOR(ID, NAME, SURNAME) values (2, 'Ivan', 'Ivanov');
+insert into AUTHOR(ID, NAME, SURNAME) values (3, 'John', 'Snow');
+
+insert into GENRE(ID, NAME) values (1, 'Mystic');
+insert into GENRE(ID, NAME) values (2, 'Fantasy');
+
+insert into BOOK(ID, NAME, AUTHOR_ID, GENRE_ID) values (1, 'Who are you?', 1, 1);
+insert into BOOK(ID, NAME, AUTHOR_ID, GENRE_ID) values (2, '2020 is the past', 1, 2);
+insert into BOOK(ID, NAME, AUTHOR_ID, GENRE_ID) values (3, 'Goblins parade', 2, 2);
+insert into BOOK(ID, NAME, AUTHOR_ID, GENRE_ID) values (4, 'My life is done', 3, 1);
