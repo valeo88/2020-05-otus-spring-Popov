e84aef614a3f2f93339b952dbf07072e61c48e1c
Valentin Popov <VOPopov@luxoft.com>
2020-09-13 14:57:45 +0300

    dbrefs

--- hw11-library-webflux/src/main/java/ru/otus/hw11/model/Book.java
+++ hw11-library-webflux/src/main/java/ru/otus/hw11/model/Book.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -16,8 +17,10 @@ public class Book {
 
     private String name;
 
+    @DBRef
     private Author author;
 
+    @DBRef
     private Genre genre;
 
     public Book(String id, String name, Author author, Genre genre) {
