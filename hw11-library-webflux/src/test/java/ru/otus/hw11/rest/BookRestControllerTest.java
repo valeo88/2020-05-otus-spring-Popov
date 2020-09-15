dc5ff8259249ac9621762760baa9d9d415af5392
Valentin Popov <VOPopov@luxoft.com>
2020-09-15 17:11:28 +0300

    fix test

--- hw11-library-webflux/src/test/java/ru/otus/hw11/rest/BookRestControllerTest.java
+++ hw11-library-webflux/src/test/java/ru/otus/hw11/rest/BookRestControllerTest.java
@@ -1,22 +1,22 @@
 package ru.otus.hw11.rest;
 
-import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import ru.otus.hw11.dto.BookDto;
+import ru.otus.hw11.model.Author;
 import ru.otus.hw11.model.Book;
+import ru.otus.hw11.model.Genre;
 import ru.otus.hw11.service.BookService;
 
-import static reactor.core.publisher.Mono.when;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 @WebFluxTest({BookRestController.class})
-@Import({BookService.class})
 public class BookRestControllerTest {
 
     @Autowired
@@ -25,16 +25,21 @@ public class BookRestControllerTest {
     @MockBean
     private BookService bookService;
 
-
     @Test
     public void shouldReturnBooksWhenGetAll() {
-        when(bookService.getAll()).thenReturn(Flux.just(new Book("1", "1", null, null),
-                new Book("2", "2", null, null)));
+        when(bookService.getAll()).thenReturn(Flux.just(BookDto.fromBook(new Book("1", "1",
+                        new Author("1", "1"), new Genre("1", "1"))),
+                BookDto.fromBook(new Book("2", "2", new Author("1", "1"), new Genre("1", "1")))));
 
         webClient.get()
                 .uri("/api/book")
                 .exchange()
                 .expectStatus()
                 .isOk();
+    }
+
+    @Configuration
+    @Import({BookService.class, BookRestController.class})
+    static class Config {
     }
 }
